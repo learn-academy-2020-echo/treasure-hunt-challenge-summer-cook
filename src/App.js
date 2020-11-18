233c916f8092ddfec640094056ac053923a03fdd
summer-cook <summercook@bellsouth.net>
2020-11-18 10:56:13 -0800

    add board game UI & mapped component

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react'
+import Square from './components/Square'
 import './App.css'
 
 class App extends Component{
@@ -12,8 +13,22 @@ class App extends Component{
   render(){
     return(
       <React.Fragment>
+      <center>
         <h1>Treasure Hunt App</h1>
+        <div id="gameboard">
+        { this.state.squares.map((value, index) => {
+          return (
+            <Square
+              value = { value }
+              key= { index }
+            />
+          )
+        }) }
+        </div>
+        </center>
       </React.Fragment>
+      //you have to add the key value pair inside the square component call in the render in order to refer to this.props.value and pull it into the square component
+      //need to wrap ALL the square (and all the map logic) inside a div so that we can then style that div.
     )
   }
 }
--- src/components/Square.js
+++ src/components/Square.js
@@ -1,11 +1,16 @@
 import React, { Component } from 'react'
 
+//we are going to make an instance of a single square, and then we are gonna render that square 9 times
 class Square extends Component{
   render(){
     return(
       <React.Fragment>
+      <div id="square">{ this.props.value }
+      </div>
       </React.Fragment>
     )
   }
 }
 export default Square
+
+// you have to add the inner HTML in order to see a ? (the string) inside every box
